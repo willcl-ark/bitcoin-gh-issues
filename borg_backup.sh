e91455853fa42cbc4670d041ef8695cbf28b6ca6
willcl-ark <will@256k1.dev>
2023-04-15 17:21:43 +0100

    Use borgbackup for db backup
    de-dupicates etc. automagically

---
+++ borg_backup.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO=/home/ubuntu/bitcoin-github/db_backups
+
+# See the section "Passphrase notes" for more infos.
+export BORG_PASSPHRASE='password1234'
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$(date)" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Starting backup"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+borg create \
+	--verbose \
+	--filter AME \
+	--list \
+	--stats \
+	--show-rc \
+	--compression lz4 \
+	--exclude-caches \
+	\
+	::'{hostname}-{now}' \
+	/home/ubuntu/bitcoin-github/issues.db
+
+backup_exit=$?
+
+info "Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-*' matching is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+borg prune \
+	--list \
+	--glob-archives '{hostname}-*' \
+	--show-rc \
+	--keep-minutely 10 \
+	--keep-hourly 6 \
+	--keep-daily 7 \
+	--keep-weekly 4 \
+	--keep-monthly 6
+
+prune_exit=$?
+
+# actually free repo disk space by compacting segments
+
+info "Compacting repository"
+
+borg compact
+
+compact_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$((backup_exit > prune_exit ? backup_exit : prune_exit))
+global_exit=$((compact_exit > global_exit ? compact_exit : global_exit))
+
+if [ ${global_exit} -eq 0 ]; then
+	info "Backup, Prune, and Compact finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+	info "Backup, Prune, and/or Compact finished with warnings"
+else
+	info "Backup, Prune, and/or Compact finished with errors"
+fi
+
+exit ${global_exit}
